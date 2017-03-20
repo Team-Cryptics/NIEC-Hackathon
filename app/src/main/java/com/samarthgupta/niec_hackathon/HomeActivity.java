b93410bf7445b4159d10482bf92b4d8ffb285034
aachman98 <aachman98@gmail.com>
2017-03-21 03:35:37 +0530

    Punnu buoy

--- app/src/main/java/com/samarthgupta/niec_hackathon/HomeActivity.java
+++ app/src/main/java/com/samarthgupta/niec_hackathon/HomeActivity.java
@@ -3,6 +3,8 @@ package com.samarthgupta.niec_hackathon;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,6 +14,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import me.relex.circleindicator.CircleIndicator;
 
 public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener
