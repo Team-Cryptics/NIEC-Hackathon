18c1224c1ceb2ecde94301a0ce205c1ede87e81a
Sarthak <sarthakaggarwal97@gmail.com>
2017-03-26 12:17:47 +0530

    Sarthak commit

--- app/src/main/java/com/samarthgupta/niec_hackathon/SignInActivity.java
+++ app/src/main/java/com/samarthgupta/niec_hackathon/SignInActivity.java
@@ -1,5 +1,74 @@
 package com.samarthgupta.niec_hackathon;
 
+
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class SignInActivity extends AppCompatActivity {
+
+    private static final int RC_SIGN_IN = 0;
+    private FirebaseAuth auth;
+    //Button bt1;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+        auth = FirebaseAuth.getInstance();
+        if(auth.getCurrentUser()!=null)
+        {    Log.d("Auth", auth.getCurrentUser().getEmail());
+            //already signed in
+        }else{
+            startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder().setProviders(AuthUI.FACEBOOK_PROVIDER,
+                    AuthUI.EMAIL_PROVIDER,
+                    AuthUI.GOOGLE_PROVIDER).build(),RC_SIGN_IN);
+
+        }
+      /*  bt1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        Log.d("Auth", "USER LOGGED OUT");
+                    }
+                })
+            }
+        });*/
+
+
+
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==RC_SIGN_IN)
+        {
+            if(resultCode == RESULT_OK){
+                //user logged in
+                Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
+                startActivity(intent);
+                Log.d("Auth", auth.getCurrentUser().getEmail());
+            }
+            else{
+                Log.d("Auth", "Not Authenticated");
+            }
+        }
+
+    }
+}
+
+/*
 import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.Build;
@@ -84,7 +153,7 @@ public class SignInActivity extends AppCompatActivity implements
 */
         //Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         //setSupportActionBar(toolbar);
-
+/*
 
 
         mAuth = FirebaseAuth.getInstance();
@@ -205,7 +274,7 @@ public class SignInActivity extends AppCompatActivity implements
             }
         });*/
 
-
+/*
     }
     @Override
     protected void onStart() {
@@ -263,7 +332,7 @@ public class SignInActivity extends AppCompatActivity implements
     /*public static String encodeEmail(String userEmail) {
         return userEmail.replace(".", ",");
     }*/
-
+/*
     private void FirebaseAuthWithGoogle(GoogleSignInAccount acct) {
         AuthCredential credential= GoogleAuthProvider.getCredential(acct.getIdToken(),null);
         mAuth.signInWithCredential(credential)
@@ -303,4 +372,4 @@ public class SignInActivity extends AppCompatActivity implements
 
         }
     }
-}
+*/
